b88727756948fef4dc337634fb8e1a132515388d
Daniil.Aleshechkin <daniila@iqmetrix.com>
2023-10-10 13:31:53 -0600

    Removed polling

--- src/app/components/chat.tsx
+++ src/app/components/chat.tsx
@@ -6,8 +6,6 @@ import type Prompt from "../types/prompt";
 import PromptCompoment from "./prompt";
 import sleep from "../utils/sleep";
 
-const pollsRequired: Number = 2;
-
 const Chat: React.FC = () => {
   const [prompts, setPrompts] = useState<Prompt[]>([]);
   const [inputMessage, setInputMessage] = useState<string>("");
@@ -33,42 +31,32 @@ const Chat: React.FC = () => {
   // Send prompt to get response
   useEffect(() => {
     if (currentPrompt?.responseStatus != "Waiting") {
-      return () => {};
+      return;
     }
 
     const getChatReponse = async () => {
       console.log(`Sending message: ${inputMessage}`);
       await sleep(2000);
+      const response = "Hello! thanks for your question";
+
+      let oldPrompts = [...prompts];
+      oldPrompts.push({
+        id: currentPrompt.id,
+        input: currentPrompt.input,
+        response: response,
+        responseStatus: "Success",
+      });
+      setPrompts(oldPrompts);
+
+      setCurrentPrompt({
+        id: "",
+        input: "",
+        response: null,
+        responseStatus: "Ready",
+      });
     };
 
-    let currentPolls = 0;
-
-    const pollChatResponse = setInterval(() => {
-      let response = "Hello! thanks for your question";
-
-      if (currentPolls != pollsRequired) {
-        getChatReponse();
-        currentPolls += 1;
-      } else {
-        let oldPrompts = [...prompts];
-        oldPrompts.push({
-          id: currentPrompt.id,
-          input: currentPrompt.input,
-          response: response,
-          responseStatus: "Success",
-        });
-        setPrompts(oldPrompts);
-
-        setCurrentPrompt({
-          id: "",
-          input: "",
-          response: null,
-          responseStatus: "Ready",
-        });
-      }
-    }, 5000);
-
-    return () => clearInterval(pollChatResponse);
+    getChatReponse();
   }, [currentPrompt]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
