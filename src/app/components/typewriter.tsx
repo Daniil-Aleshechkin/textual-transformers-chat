871183bbf6dbfb816cea99dcb0ef7e8d9d685e78
Daniil.Aleshechkin <daniila@iqmetrix.com>
2023-10-12 10:00:32 -0600

    Added new lines rendered

--- src/app/components/typewriter.tsx
+++ src/app/components/typewriter.tsx
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const Typewriter = ({text,delay}:{text:any, delay:any}) => {
-  const [currentText, setCurrentText] = useState('');
+const Typewriter = ({ text, delay }: { text: any; delay: any }) => {
+  const [currentText, setCurrentText] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    if (text!=null && currentIndex < text?.length) {
+    if (text != null && currentIndex < text?.length) {
       const timeout = setTimeout(() => {
-        setCurrentText(prevText => prevText + text[currentIndex]);
-        setCurrentIndex(prevIndex => prevIndex + 1);
+        setCurrentText((prevText) => text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
       }, delay);
-  
+
       return () => clearTimeout(timeout);
     }
   }, [currentIndex, delay, text]);
 
-  return <span>{currentText}</span>;
+  return <span className="whitespace-pre-line">{currentText}</span>;
 };
 
 export default Typewriter;
-
