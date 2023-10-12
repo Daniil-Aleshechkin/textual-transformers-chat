f9226676d3a69dc7c9c5f31997a4cb14f413e2df
Daniil.Aleshechkin <daniila@iqmetrix.com>
2023-10-12 13:04:43 -0600

    Fixed the rating icon

--- src/app/components/rating.tsx
+++ src/app/components/rating.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
 import { faThumbsDown } from "@fortawesome/free-regular-svg-icons/faThumbsDown";
 
-function RatingsBar(){
-    return (
+function RatingsBar() {
+  return (
     <div className="rating bg-transparent">
-      <div className="like grow">
-        <FontAwesomeIcon className="rating-icon" icon={faThumbsUp}/>
+      <div className="like grow bg-transparent">
+        <FontAwesomeIcon
+          className="rating-icon bg-transparent"
+          icon={faThumbsUp}
+        />
       </div>
-      <div className="dislike grow">
+      <div className="dislike grow  bg-transparent">
         <FontAwesomeIcon className="rating-icon" icon={faThumbsDown} />
       </div>
-  </div>
+    </div>
   );
 }
-
 
 export default RatingsBar;
